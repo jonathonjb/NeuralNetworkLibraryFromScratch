e0c8e6c9abfabb36a41fb0c8f6dda519d9c681ce
Jonathon Brandt <jonathonjb2015@gmail.com>
2020-11-19 14:58:10 -0600

    added 3d data + images

--- main.py
+++ main.py
@@ -2,79 +2,101 @@ from neural_network import NeuralNetwork
 from layer import Dense
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import random
 
-def createData():
-    # circles = np.array([[1, 5, 0], [1, 3.8, 0], [1.3, 4, 0], [2, 3.8, 0], [1.5, 1.8, 0], [0.9, 0.9, 0], [2.3, 1, 0],
-    #                     [2.6, 1.9, 0], [3.3, 1.9, 0], [4.2, 1.2, 0], [5.1, 2.1, 0], [5.4, 0.7, 0]])
-    #
-    # squares = np.array([[3, 5, 1], [3.1, 4.2, 1], [3.14, 3.1, 1], [3.9, 3.5, 1], [4.1, 4.8, 1], [4.2, 3, 1],
-    #                     [4.8, 3.1, 1], [4.8, 4.9, 1], [5, 4, 1], [5.2, 3.1, 1], [5.4, 5.1, 1], [6, 3.8, 1]])
+def create3dData():
+    circles = []
+    squares = []
 
-    circles = np.array([[1.8, 2.2, 0], [2.6, 1.8, 0], [3.8, 1.9, 0], [4, 2.1, 0], [4.1, 3, 0], [4.1, 4.1, 0],
-                        [3.9, 4.3, 0], [1.7, 4, 0], [2, 4.3, 0], [2.1, 2.1, 0], [1.8, 3, 0], [1.9, 3.2, 0],
-                        [3, 5, 0], [2.5, 4.5, 0], [2.6, 4.6, 0], [3.5, 4.6, 0], [3, 1.9, 0], [3.3, 1.8, 0], [3.5, 1.75, 0]])
-    squares = np.array([[2.2, 2.9, 1], [2.22, 3.9, 1], [3.3, 2.2, 1], [3.8, 2.65, 1], [3.8, 3.6, 1], [3.5, 4, 1],
-                        [3, 4.2, 1], [2.9, 2.4, 1], [2.2, 3.5, 1], [2.6, 2.6, 1], [3.6, 2.5, 1], [3.9, 3.1, 1],
-                        [2.6, 4, 1]])
+    for i in range(200):
+        x = random.random() * 2 + 2
+        y = random.random() * 2 + 2
+        z = random.random() * 2 + 2
+        squares.append([x, y, z, 1])
 
-    plt.scatter(circles[:, 0], circles[:, 1], marker='o', color='blue')
-    plt.scatter(squares[:, 0], squares[:, 1], marker='s', color='green')
-    plt.savefig('simpleData')
+    for i in range(500):
+        x = random.random() * 6
+        y = random.random() * 6
+        z = random.random() * 6
+
+        while((x >= 2 and x < 4) and (y >= 2 and y < 4) and (z >= 2 and z < 4)):
+            x = random.random() * 6
+            y = random.random() * 6
+            z = random.random() * 6
+
+        circles.append([x, y, z, 0])
+
+    squares = np.array(squares)
+    circles = np.array(circles)
+
+
+    figure = plt.figure()
+    ax = Axes3D(figure)
+
+    ax.scatter(squares[:, 0], squares[:, 1], squares[:, 2], marker='s', color='green')
+    ax.scatter(circles[:, 0], circles[:, 1], circles[:, 2], marker='o', color='blue')
+    plt.savefig('3dData')
     plt.show()
 
     return circles, squares
 
 # for the test set
-def createRandomData(num):
-    randData = np.random.rand(2, num) * 6
+def createRandomData3d(num):
+    randData = np.random.rand(3, num) * 6
     return randData
 
 def trainNeuralNetworkSimpleData(plotCost=False):
     # classifies if examples are squares or not
 
-    origCircles, origSquares = createData()
+    origCircles, origSquares = create3dData()
     data = np.concatenate((origCircles, origSquares), axis=0)
     X = np.delete(data, -1, axis=1)
     y = data[:, -1]
 
 
-    neuralNetwork = NeuralNetwork(inputSize=2, layers=[
+    neuralNetwork = NeuralNetwork(inputSize=3, layers=[
         Dense(size=5, activation='tanh'),
         Dense(size=5, activation='tanh'),
         Dense(size=1, activation='sigmoid')
     ])
 
-    costHistory = neuralNetwork.train(X.T, y, iterations=1000000, learningRate=0.005, printCost=True, printCostRounds=10000)
+    costHistory = neuralNetwork.train(X.T, y, iterations=500000, learningRate=0.01, printCost=True, printCostRounds=10000)
 
     if(plotCost):
         plt.plot(costHistory)
-        plt.savefig('costHistory')
+        plt.savefig('costHistory3d')
         plt.show()
 
     num = 100000
-    randData = createRandomData(num)
+    randData = createRandomData3d(num)
     predictions = neuralNetwork.predict(randData)
 
+    plotResults3d(neuralNetwork, num, origCircles, origSquares, predictions, randData)
+
+def plotResults3d(neuralNetwork, num, origCircles, origSquares, predictions, randData):
     squares = []
     circles = []
     for i in range(num):
-        if(predictions[0, i] == 1):
+        if (predictions[0, i] == 1):
             squares.append(i)
         else:
             circles.append(i)
-
     squares = randData[:, squares]
     circles = randData[:, circles]
 
-    plt.scatter(squares[0], squares[1], marker='s', color='springgreen')
-    plt.scatter(circles[0], circles[1], marker='o', color='cornflowerblue')
+    figure = plt.figure()
+    ax = Axes3D(figure)
 
-    plt.scatter(origCircles[:, 0], origCircles[:, 1], marker='o', color='blue')
-    plt.scatter(origSquares[:, 0], origSquares[:, 1], marker='s', color='green')
-    plt.savefig('endResult')
+    ax.scatter(squares[0], squares[1], squares[2], marker='s', color='springgreen')
+    ax.scatter(circles[0], circles[1], circles[2], alpha=0.01, marker='o', color='cornflowerblue')
+    ax.scatter(origCircles[:, 0], origCircles[:, 1], origCircles[:, 2], marker='o', color='blue')
+    ax.scatter(origSquares[:, 0], origSquares[:, 1], origSquares[:, 2], marker='s', color='green')
+    plt.savefig('endResult3d')
     plt.show()
 
     neuralNetwork.prettyPrint()
+
 
 def main():
     trainNeuralNetworkSimpleData(plotCost=True)
