8da6377d7f29dc4e23228167cac08c69825677f2
Jonathon Brandt <jonathonjb2015@gmail.com>
2020-11-18 21:39:05 -0600

    Added new set of examples, plotted the original data over the end result for better visualization

--- main.py
+++ main.py
@@ -4,39 +4,47 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def createData():
-    circles = np.array([[1, 5, 0], [1, 3.8, 0], [1.3, 4, 0], [2, 3.8, 0], [1.5, 1.8, 0], [0.9, 0.9, 0], [2.3, 1, 0],
-                        [2.6, 1.9, 0], [3.3, 1.9, 0], [4.2, 1.2, 0], [5.1, 2.1, 0], [5.4, 0.7, 0]])
+    # circles = np.array([[1, 5, 0], [1, 3.8, 0], [1.3, 4, 0], [2, 3.8, 0], [1.5, 1.8, 0], [0.9, 0.9, 0], [2.3, 1, 0],
+    #                     [2.6, 1.9, 0], [3.3, 1.9, 0], [4.2, 1.2, 0], [5.1, 2.1, 0], [5.4, 0.7, 0]])
+    #
+    # squares = np.array([[3, 5, 1], [3.1, 4.2, 1], [3.14, 3.1, 1], [3.9, 3.5, 1], [4.1, 4.8, 1], [4.2, 3, 1],
+    #                     [4.8, 3.1, 1], [4.8, 4.9, 1], [5, 4, 1], [5.2, 3.1, 1], [5.4, 5.1, 1], [6, 3.8, 1]])
 
-    squares = np.array([[3, 5, 1], [3.1, 4.2, 1], [3.14, 3.1, 1], [3.9, 3.5, 1], [4.1, 4.8, 1], [4.2, 3, 1],
-                        [4.8, 3.1, 1], [4.8, 4.9, 1], [5, 4, 1], [5.2, 3.1, 1], [5.4, 5.1, 1], [6, 3.8, 1]])
+    circles = np.array([[1.8, 2.2, 0], [2.6, 1.8, 0], [3.8, 1.9, 0], [4, 2.1, 0], [4.1, 3, 0], [4.1, 4.1, 0],
+                        [3.9, 4.3, 0], [1.7, 4, 0], [2, 4.3, 0], [2.1, 2.1, 0], [1.8, 3, 0], [1.9, 3.2, 0],
+                        [3, 5, 0], [2.5, 4.5, 0], [2.6, 4.6, 0], [3.5, 4.6, 0], [3, 1.9, 0], [3.3, 1.8, 0], [3.5, 1.75, 0]])
+    squares = np.array([[2.2, 2.9, 1], [2.22, 3.9, 1], [3.3, 2.2, 1], [3.8, 2.65, 1], [3.8, 3.6, 1], [3.5, 4, 1],
+                        [3, 4.2, 1], [2.9, 2.4, 1], [2.2, 3.5, 1], [2.6, 2.6, 1], [3.6, 2.5, 1], [3.9, 3.1, 1],
+                        [2.6, 4, 1]])
 
     plt.scatter(circles[:, 0], circles[:, 1], marker='o', color='blue')
     plt.scatter(squares[:, 0], squares[:, 1], marker='s', color='green')
     plt.savefig('simpleData')
     plt.show()
 
-    data = np.concatenate((circles, squares), axis=0)
-    return data
+    return circles, squares
 
 # for the test set
 def createRandomData(num):
-    randData = np.random.rand(2, num) * 5.5
+    randData = np.random.rand(2, num) * 6
     return randData
 
 def trainNeuralNetworkSimpleData(plotCost=False):
     # classifies if examples are squares or not
 
-    data = createData()
-    X = np.delete(data, 2, axis=1)
+    origCircles, origSquares = createData()
+    data = np.concatenate((origCircles, origSquares), axis=0)
+    X = np.delete(data, -1, axis=1)
     y = data[:, -1]
 
+
     neuralNetwork = NeuralNetwork(inputSize=2, layers=[
-        Dense(size=3, activation='tanh'),
-        Dense(size=3, activation='tanh'),
+        Dense(size=5, activation='tanh'),
+        Dense(size=5, activation='tanh'),
         Dense(size=1, activation='sigmoid')
     ])
 
-    costHistory = neuralNetwork.train(X.T, y, iterations=10000, learningRate=0.05, printCost=True, printCostRounds=1000)
+    costHistory = neuralNetwork.train(X.T, y, iterations=1000000, learningRate=0.005, printCost=True, printCostRounds=10000)
 
     if(plotCost):
         plt.plot(costHistory)
@@ -58,8 +66,11 @@ def trainNeuralNetworkSimpleData(plotCost=False):
     squares = randData[:, squares]
     circles = randData[:, circles]
 
-    plt.scatter(squares[0], squares[1], marker='s', color='green')
-    plt.scatter(circles[0], circles[1], marker='o', color='blue')
+    plt.scatter(squares[0], squares[1], marker='s', color='springgreen')
+    plt.scatter(circles[0], circles[1], marker='o', color='cornflowerblue')
+
+    plt.scatter(origCircles[:, 0], origCircles[:, 1], marker='o', color='blue')
+    plt.scatter(origSquares[:, 0], origSquares[:, 1], marker='s', color='green')
     plt.savefig('endResult')
     plt.show()
 
